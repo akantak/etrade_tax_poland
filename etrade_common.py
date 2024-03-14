23350c7792e562f36370269f176c3e5830f2644a
Aleksander Kantak <aleksander.kantak@intel.com>
2024-03-14 17:21:30 +0100

    Cache locally NBP ratios

--- etrade_common.py
+++ etrade_common.py
@@ -1,6 +1,7 @@
 """Implement common functions for etrade documents processing."""
 import datetime
 import glob
+import json
 import os
 from time import sleep
 
@@ -8,7 +9,6 @@ from pypdf import PdfReader
 
 import requests
 
-NBP_URL = 'https://api.nbp.pl/api/exchangerates/rates/a/usd/{}/?format=json'
 TAX_PL = 0.19
 
 
@@ -27,26 +27,72 @@ def file_to_text(filename):
     return text
 
 
-def date_to_usd_pln(date_obj):
-    """Find 'day before vestment' USD/PLN ratio."""
-    date_obj -= datetime.timedelta(days=1)
-    while True:
-        try:
-            req = requests.get(NBP_URL.format(date_obj.strftime('%Y-%m-%d')), timeout=5)
-        except requests.exceptions.ConnectTimeout:
-            print('Failed getting USD/PLN ratio in 5 seconds, retrying after 1 second')
+class NbpRatiosCache():
+    """Cache data instead of always requesting from NBP."""
+
+    def __init__(self):
+        """Initialize objects and fields."""
+        self.date_format = '%Y-%m-%d'
+        self.nbp_url = 'https://api.nbp.pl/api/exchangerates/rates/a/usd/{}/?format=json'
+        self.cache_file = f'{os.path.dirname(os.path.abspath(__file__))}/nbp_cache.json'
+        self.read_cache()
+
+    def read_cache(self):
+        """Read cache file."""
+        if not os.path.isfile(self.cache_file):
+            self.cache = {}
+            return
+        with open(self.cache_file, 'r', encoding='utf-8') as file:
+            self.cache = json.load(file)
+
+    def write_cache(self):
+        """Write cache file."""
+        with open(self.cache_file, 'w', encoding='utf-8') as file:
+            json.dump(self.cache, file, sort_keys=True, indent=2, separators=(',', ': '))
+
+    def get_ratio(self, date_obj):
+        """Get ratio from cache if available, otherwise request from NBP."""
+        key = date_obj.strftime(self.date_format)
+        if key in self.cache:
+            if not self.cache[key]:
+                raise ValueError('Ratio for selected date is not available in NBP')
+            return self.cache[key]
+
+        while True:
+            try:
+                current_url = self.nbp_url.format(date_obj.strftime(self.date_format))
+                req = requests.get(current_url, timeout=5)
+            except (requests.exceptions.ProxyError, requests.exceptions.ConnectTimeout):
+                print('Timeout 5 seconds when getting USD/PLN ratio, retrying after 1 second')
+                sleep(1)
+                continue
+            if req.status_code == 200:
+                self.cache[key] = req.json()['rates'][0]['mid']
+                self.write_cache()
+                return self.cache[key]
+            if req.status_code == 404 and req.text == '404 NotFound - Not Found - Brak danych':
+                self.cache[key] = ''
+                self.write_cache()
+                raise ValueError('Ratio for selected date is not available in NBP')
+            print(f'{req.status_code} {req.text}')
+            print('Unhandled error when getting USD/PLN ratio, retrying after 1 second')
             sleep(1)
             continue
-        if req.status_code == 200:
-            return (
-                date_obj,
-                req.json()['rates'][0]['mid'],
-            )
-        if req.status_code == 404 and req.text == '404 NotFound - Not Found - Brak danych':
+
+
+NBP_CACHE = NbpRatiosCache()
+
+
+def date_to_usd_pln(date_obj):
+    """Find 'day before vestment' USD/PLN ratio."""
+    while True:
+        date_obj -= datetime.timedelta(days=1)
+        try:
+            ratio = NBP_CACHE.get_ratio(date_obj)
+            break
+        except ValueError:
             date_obj -= datetime.timedelta(days=1)
-        else:
-            print(f'{req.status_code} {req.text}')
-            raise NotImplementedError('Not handled case during getting USD/PLN ratio')
+    return (date_obj, ratio)
 
 
 def save_csv(filename, header, lines):
---
+++ util_fill_nbp_cache.py
@@ -0,0 +1,12 @@
+"""Fill the NBP cache."""
+import datetime
+
+from etrade_common import date_to_usd_pln
+
+date_obj = datetime.datetime.now()
+end = datetime.datetime.fromisoformat('2020-01-01')
+while True:
+    if date_obj < end:
+        break
+    date_to_usd_pln(date_obj)
+    date_obj -= datetime.timedelta(days=1)
