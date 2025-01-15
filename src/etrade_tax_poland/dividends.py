0c07215463c8fca3c4d75d673b1e27703198356e
Aleksander Kantak <ajkantak@gmail.com>
2025-01-15 11:45:20 +0100

    Fix dividends gather from a split table

--- src/etrade_tax_poland/dividends.py
+++ src/etrade_tax_poland/dividends.py
@@ -56,49 +56,59 @@ class Dividend:
         self.pln_tax_due = self.flat_rate_tax - self.pln_tax_paid
 
 
-def get_stock_dividend_from_text(text):
+def get_stock_dividends_from_text(text):
     """Get dividend data from text."""
-    dividend_lines = []
+    dividend_lines_starts = []
     year_line = ""
+    dividends = []
     lines = text.split("\n")
 
     for i, line in enumerate(lines):
         if "Dividend " in line and "Next Dividend Payable" not in line and "LIQUIDITY" not in line:
-            dividend_lines = lines[i : i + 6]
+            dividend_lines_starts.append(i)
         if "Account DetailCLIENT STATEMENT" in line:
             # 'Account DetailCLIENT STATEMENT     For the Period September 1 -30, 2023'
             year_line = line
 
-    if not dividend_lines:
-        return {}
+    if not dividend_lines_starts:
+        return []
 
-    if "Qualified" in dividend_lines[0]:
-        # latest 2023 doc version, example:
-        # [0] '12/1 Qualified Dividend INTEL CORP 125.00'
-        # [1] '12/1 Tax Withholding INTEL CORP (18.75)'
-        # [2] '12/4 Funds Transferred WIRE OUT (106.25)'
-        str_date = f"{dividend_lines[0].split()[0]}/{year_line.split()[-1]}"
-        pay_date = datetime.strptime(str_date, "%m/%d/%Y")
-        gross = cash_float(dividend_lines[0].split()[-1])
-        tax = cash_float(dividend_lines[1].split()[-1])
-        net = cash_float(dividend_lines[2].split()[-1])
-    else:
-        # before 09.2023 doc version, example:
-        # [0] '03/01/23 Dividend INTEL CORP'
-        # [1] 'CASH DIV  ON     264 SHS'
-        # [2] 'REC 02/07/23 PAY 03/01/23'
-        # [3] 'NON-RES TAX WITHHELD @ .15000INTC 54.75 365.00'
-        # [4] 'TOTALDIVIDENDS&INTERESTACTIVITY $54.75 $365.00'
-        # [5] 'NETDIVIDENDS&INTERESTACTIVITY $310.25'
-        date = dividend_lines[0].split()[0]
-        pay_date = datetime.strptime(f"{date[:-2]}20{date[-2:]}", "%m/%d/%Y")
-        gross = cash_float(dividend_lines[3].split()[-1])
-        tax = cash_float(dividend_lines[3].split()[-2])
-        net = cash_float(dividend_lines[5].split()[-1])
+    for dividend_lines_start in dividend_lines_starts:
+        if "Qualified Dividend" in lines[dividend_lines_start]:
+            # latest 2023 doc version, example:
+            # [0] '12/1 Qualified Dividend INTEL CORP 125.00'
+            # [1] '12/1 Tax Withholding INTEL CORP (18.75)'
+            offset = dividend_lines_start
+            str_date = f"{lines[offset].split()[0]}/{year_line.split()[-1]}"
+            pay_date = datetime.strptime(str_date, "%m/%d/%Y")
+            gross = cash_float(lines[offset].split()[-1])
 
-    dividend = Dividend(pay_date, gross, tax, net)
-    dividend.insert_currencies_ratio()
-    return dividend
+            # depends on the file, table can be continued on the next page
+            while offset < len(lines) and "Tax Withholding" not in lines[offset]:
+                offset += 1
+            if offset == len(lines):
+                print(f"Did not found all dividend fields for {gross} gross value")
+                continue
+            tax = cash_float(lines[offset].split()[-1])
+            net = gross - tax
+        else:
+            # before 09.2023 doc version, example:
+            # [0] '03/01/23 Dividend INTEL CORP'
+            # [1] 'CASH DIV  ON     264 SHS'
+            # [2] 'REC 02/07/23 PAY 03/01/23'
+            # [3] 'NON-RES TAX WITHHELD @ .15000INTC 54.75 365.00'
+            # [4] 'TOTALDIVIDENDS&INTERESTACTIVITY $54.75 $365.00'
+            # [5] 'NETDIVIDENDS&INTERESTACTIVITY $310.25'
+            dividend_lines = lines[dividend_lines_start : dividend_lines_start + 6]
+            date = dividend_lines[0].split()[0]
+            pay_date = datetime.strptime(f"{date[:-2]}20{date[-2:]}", "%m/%d/%Y")
+            gross = cash_float(dividend_lines[3].split()[-1])
+            tax = cash_float(dividend_lines[3].split()[-2])
+            net = cash_float(dividend_lines[5].split()[-1])
+        dividend = Dividend(pay_date, gross, tax, net)
+        dividend.insert_currencies_ratio()
+        dividends.append(dividend)
+    return dividends
 
 
 def get_liquidity_dividends_from_text(text):
@@ -132,9 +142,10 @@ def process_dividend_docs(directory, debug=False):
     dividends = []
     for filename in files:
         text = fh.file_to_text(f"{directory}/{filename}")
-        if dividend := get_stock_dividend_from_text(text):
-            dividend.file = filename
-            dividends.append(dividend)
+        if divs := get_stock_dividends_from_text(text):
+            for div in divs:
+                div.file = filename
+            dividends += divs
         if ldivs := get_liquidity_dividends_from_text(text):
             for ldiv in ldivs:
                 ldiv.file = filename
